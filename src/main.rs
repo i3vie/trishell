75850d845df978708df573993e2657579e66bb78
i3vie <180745311+i3vie@users.noreply.github.com>
2024-09-24 18:57:22 -0500

    basic shell functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::{self, ErrorKind, Write};
+use std::process::{Command, exit};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+
+        if input == "exit" {
+            exit(0);
+        }
+
+        let parts: Vec<&str> = input.split_whitespace().collect();
+        if let Some((command, args)) = parts.split_first() {
+            let status = Command::new(command)
+                .args(args)
+                .status();
+
+            if let Err(e) = status {
+                if e.kind() == ErrorKind::NotFound {
+                    eprintln!("trishell: command not found: {}", command)
+                } else {
+                    eprintln!("Failed to execute command: {}", e.kind());
+                }
+            }
+        }
+    }
 }
