21e2b046d7d7908d08668a6fa153468d82361ca2
i3vie <180745311+i3vie@users.noreply.github.com>
2024-10-06 20:29:25 -0500

    fix crash with $d on navigating to root

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
-use std::fs;
+use std::{ffi::OsStr, fs};
 
+use dirs::home_dir;
 use users::get_current_username;
 use yaml_rust::{Yaml, YamlLoader};
 
@@ -15,7 +16,7 @@ const DEFAULT_CONFIG: &str = "
 # $d is the current directory name only
 # $$ is a literal $
 
-prompt_format: \"$U@$H $$ \"
+prompt_format: \"$U@$H [$D] $$ \"
 
 ";
 
@@ -34,9 +35,27 @@ pub fn get_config() -> Yaml {
 pub fn parse_prompt(config: &Yaml) -> String {
     let prompt_format = config["prompt_format"].as_str().unwrap();
 
+    let current_dir = std::env::current_dir().unwrap();
+    let home = home_dir().unwrap();
+    let home_string = home.to_str().unwrap();
+
+    let mut full_dir = String::from(current_dir.as_os_str().to_str().unwrap());
+
+    let mut front_dir = String::from(
+        current_dir
+            .file_name()
+            .unwrap_or(OsStr::new("/"))
+            .to_str()
+            .unwrap()
+    );
+
+    full_dir = full_dir.replace(&home_string, "~");
+    front_dir = front_dir.replace(&home_string, "~");
+
     let mut prompt = prompt_format.replace("$U", get_current_username().unwrap().to_str().unwrap());
     prompt = prompt.replace("$H", fs::read_to_string("/proc/sys/kernel/hostname").unwrap().trim());
-    prompt = prompt.replace("$D", std::env::current_dir().unwrap().as_os_str().to_str().unwrap());
+    prompt = prompt.replace("$D", &full_dir);
+    prompt = prompt.replace("$d", &front_dir);
     prompt = prompt.replace("$$", "$");
 
     prompt
